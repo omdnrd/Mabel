7c5028301efb15fa37e0869ebe30c08fa2c0597e
Yeminem <omdnrd@gmail.com>
2022-05-07 01:04:02 -0500

    added middleware for cors header

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const url = 'mongodb+srv://admin:AclvbpmtzXBZcr13@cluster0.avseh.mongodb.net/app
 
 const server = express()
 
+//middlewear to append cors headers to responses
+//update to match the domain you will make the request from
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 mongoose.connect(url)
 const con = mongoose.connection
@@ -14,8 +22,13 @@ con.on('open', () => {
     console.log('Connected!')
 })
     
-server.use(express.json())
+server.use(express.json());
+//not sure what this is for
+server.use(express.urlencoded({
+    extended: true
+}));
 
+//see if i should use somehting else instead of '/api/family'
 const familyRouter = require('./routes/familyRoute')
 server.use('/api/family', familyRouter)
 
